38c3bd9a8ccb9f1124f074b315a9ad5788241af1
Krriya <128789966+Krriyash@users.noreply.github.com>
2023-09-28 16:04:31 +0530

    Updated add_book.py

--- LM/sourcecode/add_book.py
+++ LM/sourcecode/add_book.py
@@ -18,8 +18,22 @@ def add_book():
 
     add_window = tk.Toplevel()
     add_window.title("Add Book")
-    add_window.geometry("1024x768")  # Set window dimensions to 1024x768
+    
+    # Calculate window dimensions
+    window_width = 400
+    window_height = 240
+    
+    # Get the screen width and height
+    screen_width = add_window.winfo_screenwidth()
+    screen_height = add_window.winfo_screenheight()
+    
+    # Calculate x and y coordinates for the centered window
+    x = (screen_width // 2) - (window_width // 2)
+    y = (screen_height // 2) - (window_height // 2)
+    
+    add_window.geometry(f"{window_width}x{window_height}+{x}+{y}")  # Center the window
 
+    # Create labels and entries using the pack layout with centered alignment
     title_label = tk.Label(add_window, text="Title:")
     title_label.pack()
     title_entry = tk.Entry(add_window)
@@ -50,3 +64,11 @@ def add_book():
 
     # Bind Enter key to save_book function
     add_window.bind('<Return>', lambda event=None: save_book())
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.title("Library Management System")
+    root.geometry("1024x768")
+    root.resizable(False, False)
+
+    root.mainloop()
